11189b4a7c1a10fec5c1f3c5a18ac7673b26978f
Yash Tripathi <yashdatttripathi@gmail.com>
2025-04-01 00:23:20 +0530

    defining App routes

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -6,13 +6,42 @@ import {
   Route,
   Navigate,
 } from 'react-router-dom';
+import Login from './pages/Auth/Login';
+import SignUp from './pages/Auth/SignUp';
+import Home from './pages/Dashboard/Home';
+import Income from './pages/Dashboard/Income';
+import Expense from './pages/Dashboard/Expense';
+
+
+
 
 const App = () => {
   return (
     <div >
-      
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root/>} />
+          <Route path="/login" exact element={<Login/>} />
+          <Route path="/signUp" exact element={<SignUp/>} />
+          <Route path="/dashboard" exact element={<Home/>} />
+          <Route path="/income" exact element={<Income/>} />
+          <Route path="/expense" exact element={<Expense/>} />
+
+        </Routes>
+      </Router>
     </div>
   )
 }
 
-export default App
+export default App;
+
+const Root = () => {
+
+  const isAuthenticated = !!localStorage.getItem('token');
+
+  return isAuthenticated ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to="/login" />
+  );
+};
